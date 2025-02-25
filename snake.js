71072473a903bea1c87e30cdb2fc74f78774ea9e
nono-de-coco <noahmulton@gmail.com>
2025-02-25 13:45:02 +0000

    separation html css js

---
+++ snake.js
@@ -0,0 +1,6 @@
+  // let counter = 2;
+  // console.log("bonjour " + counter);
+  let canvas = document.querySelector("canvas");
+  let ctx = canvas.getContext("2d");
+  ctx.fillStyle = "blue";
+  ctx.fillRect(10,10,100,100)
