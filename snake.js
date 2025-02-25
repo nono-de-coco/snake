b114c20cb695a6d91e72941d53ce4858acb277d9
nono-de-coco <noahmulton@gmail.com>
2025-02-25 14:22:33 +0000

    lancement du jeu

--- snake.js
+++ snake.js
@@ -1,6 +1,19 @@
-  // let counter = 2;
-  // console.log("bonjour " + counter);
-  let canvas = document.querySelector("canvas");
-  let ctx = canvas.getContext("2d");
-  ctx.fillStyle = "blue";
-  ctx.fillRect(10,10,100,100)
+// let counter = 2;
+// console.log("bonjour " + counter);
+let canvas = document.querySelector("canvas");
+let ctx = canvas.getContext("2d");
+//ctx.fillStyle = "blue";
+// ctx.fillRect(10,10,100,100)
+
+let gamestarted=false
+
+function gameloop() {
+    if(gamestarted){
+
+    }else{
+        ctx.font="48px serif"
+        ctx.fillText("press space to launch",10,50)
+    }
+}
+
+gameloop();
